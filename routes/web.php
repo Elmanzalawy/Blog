ab1425ed0f8f4e8cab670be94692356b9af87aab
Abdelrahmanmoussan <abdelrahmanmoussan339@gmail.com>
2024-03-14 13:50:52 +0200

    add athurauthentication on blog

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\foodcontroller;
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,18 +14,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
-Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 
-Route::get('/posts/{posts}/edit', [PostController::class,'edit'])->name('posts.edit');
 
-Route::get('/posts/{posts}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::middleware('auth')->group(function () {
 
-Route::put('/posts/{posts}', [PostController::class, 'update'])->name('posts.update');
 
-Route::delete('/posts/{posts}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+
+    Route::get('/posts/{posts}/edit', [PostController::class,'edit'])->name('posts.edit');
+
+    Route::get('/posts/{posts}', [PostController::class, 'show'])->name('posts.show');
+
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+    Route::put('/posts/{posts}', [PostController::class, 'update'])->name('posts.update');
+
+    Route::delete('/posts/{posts}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
