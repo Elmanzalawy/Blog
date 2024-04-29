5c97d3027c568ae1acac16878f62d8c6474b4d3a
Abdelrahmanmoussan <abdelrahmanmoussan339@gmail.com>
2024-04-29 12:28:21 +0300

    add if

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,7 @@
     </x-slot>
 
 
-
+@if (Session::has('status'))
     <div class="alert alert-success alert-dismissible fade show" role="alert">
         <strong>Success!</strong> {{Session::get('status')}}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
